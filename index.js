948afbdcc3cc04f4b372f54a11862b4b2a9a28ad
guilhermeredua <127844688+guilhermeredua@users.noreply.github.com>
2024-08-29 01:31:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let anoAtual = 2024;
+const nomeCompleto = "Guilherme Henrique Diniz Gemeno Rédua";
+let idade = 17;
+const cursoFaculdade = "Administração";
+const cidadeAtual = "São José dos Pinhais";
+
+console.log(`Olá eu sou o ${nomeCompleto} eu tenho ${idade} anos, moro em ${cidadeAtual}, ano que vem quero estar cursando ${cursoFaculdade}`);
+
+anoAtual = 2025;
+let idadeFutura = 19
+
+console.log(`Olá eu sou o ${nomeCompleto} eu tenho ${idadeFutura}, moro em ${cidadeAtual} e curso ${cursoFaculdade}`); 
