1bf17a61c002c8db2494f8a3535369d250b028db
guilhermeredua <127844688+guilhermeredua@users.noreply.github.com>
2024-09-19 01:34:45 +0000

    18/09/2024

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ anoAtual = 2025;
 let idadeFutura = 19
 
 console.log(`Olá eu sou o ${nomeCompleto} eu tenho ${idadeFutura}, moro em ${cidadeAtual} e curso ${cursoFaculdade}`); 
+
+//Agência de viagens
+
+const nomeComprador = "Adilson";
+const cpfComprador = 123456789;
+const idadeComprador = 12;
+const listaDestinos = new Array(
+ " Viena",
+ " Varsóvia",
+ " Berlim",
+ " Braunau Am Inn",
+ " e Praga"  
+);
+
+console.log(`Olá Sr. ${nomeComprador} portador do CPF ${cpfComprador}, a compra das passagens de avião com os destinos ${listaDestinos} foi confirmada.`);
+
+console.log(listaDestinos[0]);
+console.log("");
+
+console.log(`Olá senhor ${nomeComprador}, adicionamos uma nova viagem em sua lista de interesses.`);
+
+listaDestinos.push("Salzburgo");
+console.log(listaDestinos);
